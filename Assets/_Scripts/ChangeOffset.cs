83be9497dbfec4e583bfa8ce9d420eec851edce9
Faris AlShehri <faris@shehri.me>
2018-08-03 03:21:15 +0300

    Fixed VR interaction and added heatmap function

---
+++ Assets/_Scripts/ChangeOffset.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeOffset : MonoBehaviour {
+
+    public Renderer rend;
+    public Material mat;
+
+    public float offset;
+
+    // Use this for initialization
+    void Start () {
+        rend = gameObject.GetComponent<Renderer>();
+        mat = rend.material;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        //mat.mainTextureOffset = new Vector2(offset, 0);
+        mat.SetTextureOffset("_MainTex", new Vector2(offset, 0));
+    }
+}
