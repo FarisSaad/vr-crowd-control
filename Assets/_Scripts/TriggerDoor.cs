ca7f708be940ef725f87c178b468ca9dfa0dfba6
Faris AlShehri <faris@shehri.me>
2018-08-03 07:54:51 +0300

    Added trigger

---
+++ Assets/_Scripts/TriggerDoor.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerDoor : MonoBehaviour {
+
+    public Animator anim;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            //do some stuff
+            anim.SetTrigger("open");
+        }
+
+        if (Input.GetKeyUp(KeyCode.C))
+        {
+            //do some stuff
+            anim.SetTrigger("close");
+        }
+    }
+}
